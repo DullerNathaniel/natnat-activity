6510820283682c89943132070746cbcee6ee1947
Nathaniel T. Duller <natnatduller247@gmail.com>
2024-02-19 15:03:08 +0800

    -  Added Models and Controller - php routing through controller

--- natnat-activity/routes/web.php
+++ natnat-activity/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\StudentController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(uri: '/', action: [StudentController::class, 'index']) -> name (name: 'student.index'); 
